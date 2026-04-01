e0716f9b89aa7fec298805d2741e63f81a6feb1c
MeilynMassie <massiemeilyn@gmail.com>
2026-04-01 12:33:52 -0500

    Initial commit

---
+++ src/main/java/com/mjm/api/trivia/TriviaApiApplication.java
@@ -0,0 +1,13 @@
+package com.mjm.api.trivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaApiApplication.class, args);
+    }
+
+}
